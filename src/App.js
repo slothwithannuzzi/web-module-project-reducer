7abd2f34022478d4bafe8da3f9cfbec5894400fb
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-06-16 13:05:23 -0500

    created addOne event handler and connected it to 1 onclick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, { initialState } from './reducers';
+import { addOne } from './actions';
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleAddOne = () => {
+    dispatch(addOne())
+  }
 
   return (
     <div className="App">
@@ -17,10 +25,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,7 +38,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick ={handleAddOne}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
